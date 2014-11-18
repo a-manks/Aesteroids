82c5ee5a22698f232910a6456572e0fc047fc84b
a-manks <a.manks@icloud.com>
2014-11-18 18:37:11 +1100

    Initial Commit

---
+++ SideScroller/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SideScroller
+//
+//  Created by Adam Manks on 18/11/2014.
+//  Copyright (c) 2014 Adam Manks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
